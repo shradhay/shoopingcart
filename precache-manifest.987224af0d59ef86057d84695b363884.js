ad5966a3a004ce3839cfd5967549fc70d04849a6
shradhay <shraddhayannawar16@gmail.com>
2020-10-09 01:14:01 -0700

    Updates

---
+++ precache-manifest.987224af0d59ef86057d84695b363884.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dff7a6b82ef04a0926b7d4d41977f23",
+    "url": "/shoopingcart/index.html"
+  },
+  {
+    "revision": "88a3fea63f4934240103",
+    "url": "/shoopingcart/static/css/main.fecf883c.chunk.css"
+  },
+  {
+    "revision": "c74952a7b54447f9a660",
+    "url": "/shoopingcart/static/js/2.016bcd98.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/shoopingcart/static/js/2.016bcd98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88a3fea63f4934240103",
+    "url": "/shoopingcart/static/js/main.cb1e2a06.chunk.js"
+  },
+  {
+    "revision": "86e2f68d8ba94293ed0c",
+    "url": "/shoopingcart/static/js/runtime-main.c7856292.js"
+  },
+  {
+    "revision": "c364afe759afca452bbfdbd43d40cbc4",
+    "url": "/shoopingcart/static/media/banner2.c364afe7.jpg"
+  },
+  {
+    "revision": "6876fea7f4add748e087c8417cb37682",
+    "url": "/shoopingcart/static/media/logo.6876fea7.jpg"
+  }
+]);
